9336ac08ad48532fc5151bf33563e99a40bb6747
Tiz1i <tizianamalko@gmail.com>
2022-07-26 15:32:25 +0200

    ne rregull e perfunduar

---
+++ C#/dojosurvey/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace DojoSurvey.Controllers;     //be sure to use your own project's namespace!
+public class HomeController : Controller   //remember inheritance??
+{
+    //for each route this controller is to handle:
+    [HttpGet]       //type of request
+    [Route("")]     //associated route string (exclude the leading /)
+    public ViewResult Home()
+    {
+        return View("Home");
+    }
+
+
+
+    // remember to use [HttpPost] attributes!
+    [HttpPost]
+    [Route("Result")]
+    public IActionResult  Method(string Name, string Location,string Language,string Comment)
+    {
+        
+        ViewBag.name=Name;
+        ViewBag.location=Location;
+        ViewBag.language = Language;
+        ViewBag.comment = Comment;
+        return View("Result");
+    }
+}
---
+++ C#/dojosurvey/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dojosurvey.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
