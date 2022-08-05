871c300c0e3cddb2dafe296091eba2bfcbc3ec32
Tiz1i <tizianamalko@gmail.com>
2022-08-05 12:41:30 +0200

    ok

---
+++ C#/formsub/Models/Submitter.cs
@@ -0,0 +1,27 @@
+#pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
+namespace formsub.Models;
+public class Submitter
+{
+    [Required]
+    [MinLength(3)]
+     [Display(Name="First Name")]
+    public string FirstName { get; set; }
+ 
+    [Required]
+    [Display(Name="Last Name")]
+    public string LastName { get; set; }
+
+    [Required]
+    [Display(Name="Age")]
+    public int Age{ get; set; }
+
+    [Required]
+    [Display(Name="EmailAddress")]
+    public string EmailAddress { get; set; }
+ 
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+ }
+
